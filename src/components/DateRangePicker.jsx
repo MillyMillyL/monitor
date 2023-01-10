ae3fc7c0af1ce97b4f8d0f66d41eb17131085093
JL <19100liu@gmail.com>
2023-01-10 10:08:37 -0500

    20230110

--- src/components/DateRangePicker.jsx
+++ src/components/DateRangePicker.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const DateRangePicker = () => {
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
+
   return (
     <DatePicker
       selectsRange={true}

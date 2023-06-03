972d35a2db1250574c4559259bc13eeb68f68996
hw <hw@gmail.com>
2023-06-03 17:07:13 -0400

    signalR hub update service status

--- src/pages/monitor/MonitorPage.jsx
+++ src/pages/monitor/MonitorPage.jsx
@@ -1,27 +1,100 @@
-import React, { useState } from "react";
-import useFectch from "@/hooks/useFectch";
-import { cst_MONITOR } from "@/shared/consts";
-
+import React, { useEffect, useState } from "react";
+import {
+  HubConnectionBuilder,
+  HttpTransportType,
+  HubConnectionState,
+} from "@microsoft/signalr";
 import { CircularProgress } from "@mui/material";
+
+import useFectch from "@/hooks/useFectch";
 import styles from "./MonitorPage.module.css";
+import { cst_MONITOR } from "@/shared/consts";
 
 function MonitorPage() {
   const [state, setState] = useState(null);
   const [isLoading, isSuccess, data, error] = useFectch(state, cst_MONITOR);
+  const [progressValue, setProgressValue] = useState(300);
+  const [hubData, setHubData] = useState(null);
 
-  console.log("monitor", data);
+  const url = `${import.meta.env.VITE_MONITOR_BASE_URL}/servicesMonitorHub`;
+
+  //signalR hub
+  useEffect(() => {
+    const connection = new HubConnectionBuilder()
+      .withUrl(url, {
+        skipNegotiation: true,
+        transport: HttpTransportType.WebSockets,
+      })
+      .withAutomaticReconnect()
+      .configureLogging("warn")
+      .build();
+
+    connection
+      .start()
+      .then(function () {
+        console.log("connection started");
+        connection.on(
+          "ClientServicesMonitorDataUpdate",
+          function (servicesMonitorData) {
+            // console.log("received hub data", servicesMonitorData);
+            setHubData(servicesMonitorData);
+          }
+        );
+
+        connection.onreconnecting((error) => {
+          console.log("reconnecting");
+        });
+
+        connection.onreconnected((connectionId) => {
+          console.log("reconnected");
+        });
+
+        connection.onclose((error) => {
+          console.log("closed");
+        });
+      })
+      .catch(function (err) {
+        console.error(err, "error occurred in hub connection start");
+      });
+
+    return () => {
+      if (connection && connection.state === HubConnectionState.Connected) {
+        console.log("stop connectin");
+        connection.stop();
+      }
+    };
+  }, []);
+
+  //progress bar
+  useEffect(() => {
+    let timerId = setInterval(() => {
+      setProgressValue((prev) => {
+        if (prev === 0) return 300;
+        return prev - 5;
+      });
+    }, 5000);
+
+    return () => clearInterval(timerId);
+  }, []);
+
+  // console.log("monitor", hubData ?? data, hubData, data);
   return (
     <div className={styles.container}>
       <header>
         <h1>Monitor</h1>
       </header>
       <hr />
+      <progress
+        value={progressValue}
+        max="300"
+        className={styles.progressbar}
+      ></progress>
       {isLoading ? (
         <CircularProgress />
       ) : (
         <div className={styles.servicesContainer}>
-          {data.map((e) => (
-            <div className={styles.service}>
+          {(hubData ?? data).map((e) => (
+            <div className={styles.service} key={e.id}>
               <div
                 className={`${styles.status}   ${
                   e.status === 0
